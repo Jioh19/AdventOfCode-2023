32b3a387a4f5838f2e646a9f865658eea6d96f6a
Jioh19 <jioh1982@gmail.com>
2023-12-08 12:55:28 -0300

    08 part 1 init

--- 08/main.go
+++ 08/main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+type Node struct {
+	right string
+	left  string
+}
+
+func main() {
+	fileName := "test.txt"
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	m := make(map[string]Node)
+	movement := insertData(file, m)
+	fmt.Println(movement)
+	fmt.Println(m)
+	fmt.Println(navigate(movement, m))
+}
+
+func insertData(file []byte, m map[string]Node) string {
+	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
+	for _, line := range lines[2:] {
+		key := strings.Split(strings.TrimSpace(string(line)), "=")[0]
+		key = strings.TrimSpace(key)
+		data := strings.Split(strings.TrimSpace(string(line)), "=")[1]
+		left := strings.Trim(strings.TrimSpace(strings.Split(data, ",")[0]), "(")
+		right := strings.Trim(strings.Split(data, ",")[1], ")")
+		right = strings.TrimSpace(right)
+		//fmt.Println(left, key, right)
+		node := new(Node)
+		node.left = left
+		node.right = right
+		m[key] = *node
+
+	}
+	return lines[0]
+}
+
+func navigate(movement string, m map[string]Node) int {
+	return 0
 }
