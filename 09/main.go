016e7dc15a5e20dd39a5d2de745d0392f67c2fc3
Jioh19 <jioh1982@gmail.com>
2023-12-09 10:56:29 -0300

    done 09 part 2

--- 09/main.go
+++ 09/main.go
@@ -23,7 +23,8 @@ func main() {
 	}
 	r := insertData(file)
 
-	fmt.Println(calcResult(r))
+	fmt.Println("Part 1:", calcResult(r))
+	fmt.Println("Part 2:", calcResultPrev(r))
 }
 
 func insertData(file []byte) Report {
@@ -57,10 +58,33 @@ func calcNext(h History) int {
 	return calcNext(*res) + h.data[len(h.data)-1]
 }
 
+func calcPrev(h History) int {
+	if h.data[len(h.data)-1] == 0 && h.data[0] == 0 {
+		return 0
+	}
+	res := new(History)
+	//var result = 0
+	for i := 0; i < len(h.data)-1; i++ {
+		diff := h.data[i+1] - h.data[i]
+		res.data = append(res.data, diff)
+	}
+	//fmt.Println(h.data)
+	//fmt.Println(res.data)
+	return h.data[0] - calcPrev(*res)
+}
+
 func calcResult(r Report) int {
 	total := 0
 	for _, history := range r.histories {
 		total += calcNext(history)
+	}
+	return total
+}
+
+func calcResultPrev(r Report) int {
+	total := 0
+	for _, history := range r.histories {
+		total += calcPrev(history)
 	}
 	return total
 }
