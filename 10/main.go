8977433b716af3fb0a92c494722e48ab37f2f4bf
Jioh19 <jioh1982@gmail.com>
2023-12-10 19:07:20 -0300

    done 10 part 2

--- 10/main.go
+++ 10/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type Point struct {
@@ -18,37 +19,60 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	//dir := make(map[string]Coord)
 	grid := insertData(file)
 	start := findStart(grid)
 	_ = start
 	findNext(grid, Point{0, 3})
-	result1 := navigate(grid, start)
-	fmt.Println(result1)
-	// startNode := getStart(grid)
-	// navigate(grid, startNode, *startNode.next)
-	//fmt.Println(grid, start)
+	s := time.Now()
+	result1, route := navigate(grid, start)
+	fmt.Println("Part 1:", result1)
+	//fmt.Println(route)
+	result2 := shoeLaceFormula(grid, start, route)
+	fmt.Println("Part 2:", result2)
+	fmt.Println("Time in nanoseconds:", time.Since(s).Nanoseconds())
 }
 
-func navigate(grid []string, start Point) int {
+func shoeLaceFormula(grid []string, start Point, route []Point) any {
+	// https://en.wikipedia.org/wiki/Shoelace_formula
+	polygonArea := 0
+	for i := 0; i < len(route); i++ {
+		cur := route[i]
+		next := route[(i+1)%len(route)]
+
+		polygonArea += cur.x*next.y - cur.y*next.x
+	}
+
+	if polygonArea < 0 {
+		polygonArea = -polygonArea
+	}
+	polygonArea /= 2
+
+	// https://en.wikipedia.org/wiki/Pick%27s_theorem
+	return polygonArea - len(route)/2 + 1
+}
+
+func navigate(grid []string, start Point) (int, []Point) {
 	visited := make(map[Point]int)
 	current := start
 	next := findNext(grid, start)
+	next = next[0:1]
 	result := 0
+	var route []Point
+	route = append(route, current)
 	for len(next) > 0 {
-		//fmt.Println(next)
 		visited[current] = 1
 		if visited[next[0]] == 0 {
 			result++
 			current = next[0]
+			route = append(route, current)
 			visited[next[0]] = 1
-			//fmt.Println(string(grid[next[0].y][next[0].x]), result)
 			next = append(next, findNext(grid, current)...)
 		}
 		next = next[1:]
 	}
 	result = (result + 1) / 2
-	return result
+	//fmt.Println(visited)
+	return result, route
 }
 
 func findStart(grid []string) Point {
@@ -110,13 +134,8 @@ func findNext(grid []string, pos Point) []Point {
 		points = append(points, Point{pos.x, pos.y + 1})
 		points = append(points, Point{pos.x, pos.y - 1})
 	}
-	//	fmt.Println(string(current), points)
 	return points
 }
-
-// func createDirMap(m map[string]Coord) {
-// 	m["U"]
-// }
 
 func insertData(file []byte) []string {
 	var grid []string
@@ -128,167 +147,3 @@ func insertData(file []byte) []string {
 	}
 	return grid
 }
-
-// func getStart(maze Maze) Node {
-// 	startNode := new(Node)
-// 	coord := new(Coord)
-// 	for i, pipe := range maze.pipes {
-// 		j := strings.Index(pipe, "S")
-// 		if j != -1 {
-// 			coord.i, coord.j = i, j
-// 			startNode.current = *coord
-// 			nextNode := new(Node)
-// 			prevNode := new(Node)
-// 			nextNode.prev = startNode
-// 			prevNode.next = startNode
-// 			prev := new(Coord)
-// 			next := new(Coord)
-// 			if validEntry(maze, i+1, j, "U") {
-// 				next.i = i + 1
-// 				next.j = j
-// 				nextNode.current = *next
-// 				startNode.next = nextNode
-// 			}
-// 			if validEntry(maze, i-1, j, "D") {
-// 				next.i = i - 1
-// 				next.j = j
-// 				nextNode.current = *next
-// 				startNode.next = nextNode
-// 			}
-// 			if validEntry(maze, i, j+1, "R") {
-// 				prev.i = i
-// 				prev.j = j
-// 				prevNode.current = *prev
-// 				startNode.prev = prevNode
-// 			}
-// 			if validEntry(maze, i, j-1, "L") {
-// 				prev.i = i
-// 				prev.j = j
-// 				prevNode.current = *prev
-// 				startNode.prev = prevNode
-// 			}
-// 			return *startNode
-// 		}
-// 	}
-// 	return *startNode
-// }
-
-// func navigate(maze Maze, startNode Node, currentNode Node) int {
-// 	if currentNode.next == &startNode {
-// 		return 1
-// 	}
-// 	prevNode := currentNode.prev
-// 	fmt.Println(prevNode.current)
-
-// next := new(Coord)
-// if validEntry(maze, i+1, j, "U") {
-// 	next.i = i + 1
-// 	next.j = j
-// 	nextNode.current = *next
-// 	startNode.next = nextNode
-// }
-// if validEntry(maze, i-1, j, "D") {
-// 	next.i = i - 1
-// 	next.j = j
-// 	nextNode.current = *next
-// 	startNode.next = nextNode
-// }
-// if validEntry(maze, i, j+1, "R") {
-// 	prev.i = i
-// 	prev.j = j
-// 	prevNode.current = *prev
-// 	startNode.prev = prevNode
-// }
-// if validEntry(maze, i, j-1, "L") {
-// 	prev.i = i
-// 	prev.j = j
-// 	prevNode.current = *prev
-// 	startNode.prev = prevNode
-// }
-
-//navigate(maze, startNode, *startNode.next)
-// if maze.pipes[i][j] == 'S' && maze.travel[i][j] == 1 {
-// 	return 1
-// }
-// maze.travel[i][j] = 1
-// //dir := [4]string{"U", "D", "L", "R"}
-
-// if validEntry(maze, i+1, j, "U") {
-// 	return navigate(maze, i+1, j) + 1
-// }
-// if validEntry(maze, i-1, j, "D") {
-// 	return navigate(maze, i-1, j) + 1
-// }
-// if validEntry(maze, i, j+1, "R") {
-// 	return navigate(maze, i, j+1) + 1
-// }
-// if validEntry(maze, i, j-1, "L") {
-// 	return navigate(maze, i, j-1) + 1
-// }
-// fmt.Println(maze.travel)
-// 	return 0
-// }
-
-// func validEntry(maze Maze, i int, j int, direction string) bool {
-// 	if i < 0 || i >= len(maze.pipes) || j < 0 || j >= len(maze.pipes[0]) {
-// 		return false
-// 	}
-// 	fmt.Println()
-// 	switch direction {
-// 	case "U":
-// 		if maze.pipes[i][j] == '7' || maze.pipes[i][j] == 'F' || maze.pipes[i][j] == '|' {
-// 			return true
-// 		} else {
-// 			return false
-// 		}
-// 	case "D":
-// 		if maze.pipes[i][j] == 'L' || maze.pipes[i][j] == 'J' || maze.pipes[i][j] == '|' {
-// 			return true
-// 		} else {
-// 			return false
-// 		}
-// 	case "R":
-// 		if maze.pipes[i][j] == '7' || maze.pipes[i][j] == 'J' || maze.pipes[i][j] == '-' {
-// 			return true
-// 		} else {
-// 			return false
-// 		}
-// 	case "L":
-// 		if maze.pipes[i][j] == 'L' || maze.pipes[i][j] == 'F' || maze.pipes[i][j] == '-' {
-// 			return true
-// 		} else {
-// 			return false
-// 		}
-// 	}
-// 	return false
-// }
-
-// func validExit(destination string, direction string) bool {
-// 	switch direction {
-// 	case "U":
-// 		if destination == "7" || destination == "F" || destination == "|" {
-// 			return true
-// 		} else {
-// 			return false
-// 		}
-// 	case "D":
-// 		if destination == "L" || destination == "J" || destination == "|" {
-// 			return true
-// 		} else {
-// 			return false
-// 		}
-// 	case "R":
-// 		if destination == "7" || destination == "J" || destination == "-" {
-// 			return true
-// 		} else {
-// 			return false
-// 		}
-// 	case "L":
-// 		if destination == "L" || destination == "F" || destination == "-" {
-// 			return true
-// 		} else {
-// 			return false
-// 		}
-// 	}
-// 	return false
-// }
