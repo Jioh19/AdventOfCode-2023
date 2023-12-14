68c812106d266d0a62a7d21c6d9d2b7c6bc3c60f
Jioh19 <jioh1982@gmail.com>
2023-12-14 17:40:40 -0300

    done part 2

--- 14/main.go
+++ 14/main.go
@@ -17,17 +17,43 @@ func main() {
 	grid := insertData(file)
 	s := time.Now()
 	tilted := tilt(grid)
-	// for i := 0; i < len(tilted); i++ {
-	// 	fmt.Printf("\n")
-	// 	for j := 0; j < len(tilted); j++ {
-	// 		fmt.Printf("%s", string(tilted[i][j]))
 
-	// 	}
-	// }
 	part1 := count(tilted)
 	fmt.Println("part 1:", part1)
 	fmt.Println("Time in nanoseconds:", time.Since(s).Nanoseconds())
 	//fmt.Println(grid)
+	s2 := time.Now()
+
+	rotated := rotate(tilted) //w
+	rotated = tilt2(rotated)  // w
+	rotated = rotate(rotated) //s
+	rotated = tilt2(rotated)  // s
+	rotated = rotate(rotated) //e
+	rotated = tilt2(rotated)  // e
+	rotated = rotate(rotated) //n
+	//rotated = tilt2(rotated)  // n
+
+	for i := 0; i < 999; i++ {
+
+		rotated = tilt2(rotated)
+		rotated = rotate(rotated) //w
+		rotated = tilt2(rotated)  // w
+		rotated = rotate(rotated) //s
+		rotated = tilt2(rotated)  // s
+		rotated = rotate(rotated) //e
+		rotated = tilt2(rotated)  // e
+		rotated = rotate(rotated) //n
+
+		// n
+
+	}
+
+	part2 := count(rotated)
+	part3 := (1000000000 - 1000) % 42
+
+	fmt.Println("Part 2", part2)
+	fmt.Println("Modulo", part3)
+	fmt.Println("Time in nanoseconds:", time.Since(s2).Nanoseconds())
 }
 
 func insertData(file []byte) []string {
@@ -69,6 +95,35 @@ func tilt(grid []string) [][]byte {
 	return tilted
 }
 
+func tilt2(grid [][]byte) [][]byte {
+	var tilted [][]byte
+	for i := 0; i < len(grid); i++ {
+		tilted = append(tilted, make([]byte, len(grid[i])))
+	}
+	for k := 0; k < len(grid[0]); k++ {
+		limit := 0
+		for i := 0; i < len(grid); i++ {
+			if grid[i][k] == 'O' {
+				if limit == i {
+					tilted[i][k] = 'O'
+				} else {
+					tilted[limit][k] = 'O'
+					tilted[i][k] = '.'
+				}
+				limit++
+			} else if grid[i][k] == '.' {
+				tilted[i][k] = '.'
+			} else if grid[i][k] == '#' {
+				if i < len(grid)-1 {
+					limit = i + 1
+				}
+				tilted[i][k] = '#'
+			}
+		}
+	}
+	return tilted
+}
+
 func count(tilted [][]byte) int {
 	result := 0
 	for i := 0; i < len(tilted); i++ {
@@ -79,4 +134,19 @@ func count(tilted [][]byte) int {
 		}
 	}
 	return result
+}
+
+func rotate(tilted [][]byte) [][]byte {
+	var rotated [][]byte
+	for i := 0; i < len(tilted); i++ {
+		rotated = append(rotated, make([]byte, len(tilted[i])))
+	}
+
+	for i := 0; i < len(tilted); i++ {
+		for j := 0; j < len(tilted); j++ {
+			rotated[i][len(tilted)-1-j] = tilted[j][i]
+		}
+	}
+
+	return rotated
 }
