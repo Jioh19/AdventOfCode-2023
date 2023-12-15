9a360f5eff3056ce35465eee11e8267dbf02dbb9
Jioh19 <jioh1982@gmail.com>
2023-12-15 20:39:40 -0300

    done 15 part 2

--- 15/main.go
+++ 15/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
+
+type Lens struct {
+	name  string
+	focus int
+}
 
 func main() {
 	fileName := "input.txt"
@@ -14,18 +21,15 @@ func main() {
 		return
 	}
 	input := insertData(file)
-	// for i := range input {
-	// 	for j := range input[i] {
-	// 		fmt.Printf("%s", string(input[i][j]))
 
-	// 	}
-	// }
-	// fmt.Println(input)
-	// fmt.Println()
 	s := time.Now()
 	part1 := sum(input)
 	fmt.Println("Part 1:", part1)
 	fmt.Println("Time in milliseconds:", time.Since(s).Milliseconds())
+	s2 := time.Now()
+	part2 := sum2(input)
+	fmt.Println("Part 2:", part2)
+	fmt.Println("Time in milliseconds:", time.Since(s2).Milliseconds())
 }
 
 func insertData(file []byte) [][]byte {
@@ -59,6 +63,70 @@ func sum(inputs [][]byte) int {
 		val := hash(input)
 		//fmt.Println(i, val)
 		result += val
+	}
+	return result
+}
+
+func sum2(inputs [][]byte) int {
+	result := 0
+	m := make(map[int][]Lens)
+NEXT:
+	for k, input := range inputs {
+		if len(input) > 0 {
+			arrInput := strings.Split(string(input), "=")
+			if len(arrInput) == 2 {
+				split := arrInput[0]
+
+				focus := arrInput[1]
+				com := "="
+				lens, _ := strconv.Atoi(string(focus))
+				val := hash([]byte(split))
+				fmt.Println(k, val, split, com)
+				for i, len := range m[val] {
+					if split == len.name {
+						m[val][i].focus = lens
+						continue NEXT
+					}
+				}
+				m[val] = append(m[val], Lens{string(split), lens})
+			} else {
+				split := strings.Split(string(input), "-")[0]
+				val := hash([]byte(split))
+				//fmt.Println(val, m[val], string(split))
+				for i, len := range m[val] {
+					if split == len.name {
+						//fmt.Println("Found!")
+						m[val] = append(m[val][:i], m[val][i+1:]...)
+						// if len(m[val])-1 == i {
+						// 	m[val] = m[val][:i]
+						// } else {
+						// 	m[val] = append(m[val][:i], m[val][i+1])
+
+						//}
+					}
+				}
+
+			}
+			//fmt.Println(string(split), val, string(com), int(lens))
+
+		}
+	}
+
+	// for _, box := range m {
+	// 	fmt.Println(box)
+
+	// }
+	result = calc(m)
+	return result
+}
+
+func calc(m map[int][]Lens) int {
+	result := 0
+	for i, box := range m {
+		for j, lens := range box {
+			result += (i + 1) * (j + 1) * lens.focus
+			// 	//fmt.Println(i+1, j+1, lens.focus)
+		}
 	}
 	return result
 }
